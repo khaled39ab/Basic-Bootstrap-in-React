35d4bf62e2e389f0ef0acc768c6f62081c170b2d
khaled39ab <khaled39ab@gmail.com>
2022-05-23 21:35:01 +0600

    create somethings with linking bootstrap in app.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <h1 className='bg-info p-3'>Welcome to React-Bootstrap</h1>
+      <button type="button" className="btn btn-primary">Primary</button>
+      <button type="button" className="btn btn-secondary">Secondary</button>
+      <button type="button" className="btn btn-success">Success</button>
+      <button type="button" className="btn btn-danger">Danger</button>
+      <button type="button" className="btn btn-warning">Warning</button>
+      <button type="button" className="btn btn-info">Info</button>
+      <button type="button" className="btn btn-light">Light</button>
+      <button type="button" className="btn btn-dark">Dark</button>
+
+      <button type="button" className="btn btn-link">Link</button>
     </div>
   );
 }
